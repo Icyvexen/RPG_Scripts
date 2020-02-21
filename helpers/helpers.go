e0185954a1d9780db66798c7610abd4f5afd4fe5
icyvexen <ajay.brown@ndsu.edu>
2020-02-21 12:21:49 -0600

    Test refactorings done

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -3,8 +3,11 @@ package helpers
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/Icyvexen/rpgscripts/items"
 )
 
 //TCStrings - stores Name (string), Ar(guments, []int64), and Want (string)
@@ -12,6 +15,13 @@ type TCStrings struct {
 	Name string
 	Ar   []int64
 	Want string
+}
+
+//TCMagicItems - stores Name (string), Ar(guments, []int64), and Want (MagicItem)
+type TCMagicItems struct {
+	Name string
+	Ar   []int64
+	Want items.MagicItem
 }
 
 //StringCombiner - take every incoming string, return 1 string
@@ -46,6 +56,33 @@ func LoopForTestStrings(t *testing.T, tf func(...int64) string, tests []TCString
 			} else {
 				got := tf(tt.Ar...)
 				if !strings.Contains(got, tt.Want) {
+					t.Errorf("%v --- Generated \"%v\", want \"%v\" .", tt.Name, got, tt.Want)
+				}
+			}
+		})
+	}
+}
+
+func LoopForTestMagicItems(t *testing.T, tf func(...int64) items.MagicItem, tests []TCMagicItems) {
+	t.Helper()
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			if strings.Contains(tt.Name, "Not Seeded") {
+				i := 10000
+				for i > 0 {
+					got := tf(tt.Ar...)
+					if !reflect.DeepEqual(got, tt.Want) {
+						fmt.Println("SUCCESS +++ ", tt.Name)
+						break
+					}
+					i--
+				}
+				if i == 0 {
+					t.Errorf("%v --- Could not generate \"%v\" after 10000 iterations. Maybe the test is broken?", tt.Name, tt.Want)
+				}
+			} else {
+				got := tf(tt.Ar...)
+				if !reflect.DeepEqual(got, tt.Want) {
 					t.Errorf("%v --- Generated \"%v\", want \"%v\" .", tt.Name, got, tt.Want)
 				}
 			}
--- names/ShipNames_test.go
+++ names/ShipNames_test.go
@@ -1,59 +1,26 @@
 package names_test
 
 import (
-	"fmt"
-	"reflect"
-	"strings"
 	"testing"
-	"time"
 
+	"github.com/Icyvexen/rpgscripts/helpers"
 	"github.com/Icyvexen/rpgscripts/names"
 )
 
 func TestNewShipName(t *testing.T) {
-	type args struct {
-		params []int64
-	}
 
 	//This seed is defined as such for consistent testing purposes
 	var seed int64 = 7868455476845
-	seeded := args{params: []int64{seed}}
-	notSeeded := args{}
+	seeded := []int64{seed}
+	notSeeded := []int64{}
 
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
+	tests := []helpers.TCStrings{
 		//NOTE: to validate the tests more quickly, check if 1 word from each section is present
-		{"Not Seeded - d s1", notSeeded, "Northern"},
-		{"Not Seeded - p s1", notSeeded, "Hangman's"},
-		{"Not Seeded - s1", notSeeded, "Maiden"},
-		{"Not Seeded - d s2", notSeeded, "Glory"},
-		{"seeded", seeded, "Obedient Sky's Gypsy"},
+		{Name: "Not Seeded - s1", Ar: notSeeded, Want: "Northern"},
+		{Name: "Not Seeded - s1", Ar: notSeeded, Want: "Hangman's"},
+		{Name: "Not Seeded - s1", Ar: notSeeded, Want: "Maiden"},
+		{Name: "Not Seeded - s2", Ar: notSeeded, Want: "Glory"},
+		{Name: "seeded", Ar: seeded, Want: "Obedient Sky's Gypsy"},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := names.NewShipName(tt.args.params...); !reflect.DeepEqual(got, tt.want) {
-				if strings.Contains(tt.name, "Not Seeded") {
-					for i := 10000; i > 0; i-- {
-						if !strings.Contains(got, tt.want) {
-							got = names.NewShipName()
-							time.Sleep(time.Microsecond) //give the RNG call time to use nanosecond seed
-							fmt.Println(got)
-						} else {
-							fmt.Println("CONDITION FOUND FOR", tt.name, ". NEXT!")
-							break
-						}
-					}
-					if !strings.Contains(got, tt.want) {
-						fmt.Println("Failed to find", tt.name, "**********************************************")
-						t.Errorf("Was unable to generate desired item in 10000 iterations. This may indicate a faulty test condition.")
-					}
-				} else {
-					t.Errorf("NewShipName() = %v, want %v", got, tt.want)
-				}
-			}
-		})
-	}
+	helpers.LoopForTestStrings(t, names.NewShipName, tests)
 }
